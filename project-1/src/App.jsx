0b468bf1e81623fd5aa56b28581d9c0435c167a2
Trekusov Max <maxtrekusovsite@gmail.com>
2023-10-11 17:14:25 +0300

    init

---
+++ project-1/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div
+      style={{
+        background: 'green',
+        padding: '20px',
+        textAlign: 'center',
+      }}>
+      App 1
+    </div>
+  )
+}
+
+export default App
---
+++ project-2/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div
+      style={{
+        background: 'green',
+        padding: '20px',
+        textAlign: 'center',
+      }}>
+      App 2
+    </div>
+  )
+}
+
+export default App
